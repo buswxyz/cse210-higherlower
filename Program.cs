c9ef158db99ab2d545dd36bdd351313510d90343
Stryker-Blade <jensen.d.call@gmail.com>
2022-10-07 15:12:17 -0600

    Added changes to player score
    Added the functionality so that when players get an answer right or wrong\ their score changes.

--- Program.cs
+++ Program.cs
@@ -10,17 +10,39 @@
         while(score != 0 || cardList.Count != 0){
             int selectedCard = dealer.selectCard();
             Console.WriteLine($"The current card is {previousCard}. Will the next card be HIGHER or LOWER?:");
-            string playerAnswer = Console.ReadLine();
-            
+            string? playerAnswer = Console.ReadLine();
+            if (playerAnswer == "HIGHER"){
+                if (previousCard >= selectedCard){
+                    score = score + 100;
+                    Console.WriteLine($"The card you drew was {selectedCard} which is higher than {previousCard} meaning you get 100 points, setting your total to {score}!");
+                } 
+                else{
+                    score = score - 75;
+                    Console.WriteLine($"The card you drew was {selectedCard} which is lower than {previousCard} meaning you lose 75 points, setting your total to {score}!");
+                }
+            }
+            if (playerAnswer == "LOWER"){
+                if (previousCard <= selectedCard){
+                    score = score + 100;
+                    Console.WriteLine($"The card you drew was {selectedCard} which is lower than {previousCard} meaning you get 100 points, setting your total to {score}!");
+                } 
+                else{
+                    score = score - 75;
+                    Console.WriteLine($"The card you drew was {selectedCard} which is higher than {previousCard} meaning you lose 75 points, setting your total to {score}!");
+                }
+            }
+
             cardList.RemoveAt(selectedCard);
-        }     
+               
+
+        }
+
+        bool checkDeckSize(List<string> cardList){
+
+            return true;
+        }
+
 
     }
-
-    public bool checkDeckSize(List<string> cardList){
-
-        return true;
-    }
-
 
 }
