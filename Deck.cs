16ccbba4f9649ab70dba7b3f32db115bde386707
Stryker-Blade <jensen.d.call@gmail.com>
2022-10-08 19:18:05 -0600

    More fixes
    There were some problems with how the program was reading variables so i fixed it, also changed player answer just higher for test purposes so that i could see if the program was running smoothly

--- Deck.cs
+++ Deck.cs
@@ -2,11 +2,12 @@ class Deck{
     public List<string> cards = new List<string>();
     public List<string> createDeck(List<string> cards){
 
-        for (int i = 1; i <= 13; i++){
+        for (int i = 1; i <= 13;){
             cards.Add(i.ToString());
             cards.Add(i.ToString());
             cards.Add(i.ToString());
             cards.Add(i.ToString());
+            i = i+1;
         }
 
         return cards;
--- Program.cs
+++ Program.cs
@@ -7,33 +7,35 @@
         int score = 300;
         List<string> cardList = deck.createDeck(deck.cards);
         int previousCard = 7;
-        while(score != 0 && cardList.Count != 0){
+        while(score > 0 && cardList.Count > 0){
             int selectedCard = dealer.selectCard();
+            string cardnumber= cardList[selectedCard];
+            int test = Int32.Parse(cardnumber);
             Console.WriteLine($"The current card is {previousCard}. Will the next card be HIGHER or LOWER?:");
-            string? playerAnswer = Console.ReadLine();
+            string playerAnswer = "HIGHER";
+            playerAnswer = Console.ReadLine();
+            
             if (playerAnswer == "HIGHER"){
-                if (previousCard >= selectedCard){
+                if (previousCard <= test){
                     score = score + 100;
-                    Console.WriteLine($"The card you drew was {selectedCard} which is higher than {previousCard} meaning you get 100 points, setting your total to {score}!");
+                    Console.WriteLine($"The card you drew was {test} which is higher than {previousCard} meaning you get 100 points, setting your total to {score}!");
                 } 
                 else{
                     score = score - 75;
-                    Console.WriteLine($"The card you drew was {selectedCard} which is lower than {previousCard} meaning you lose 75 points, setting your total to {score}!");
+                    Console.WriteLine($"The card you drew was {test} which is lower than {previousCard} meaning you lose 75 points, setting your total to {score}!");
                 }
             }
             if (playerAnswer == "LOWER"){
-                if (previousCard <= selectedCard){
+                if (previousCard >= test){
                     score = score + 100;
-                    Console.WriteLine($"The card you drew was {selectedCard} which is lower than {previousCard} meaning you get 100 points, setting your total to {score}!");
+                    Console.WriteLine($"The card you drew was {test} which is lower than {previousCard} meaning you get 100 points, setting your total to {score}!");
                 } 
                 else{
                     score = score - 75;
-                    Console.WriteLine($"The card you drew was {selectedCard} which is higher than {previousCard} meaning you lose 75 points, setting your total to {score}!");
+                    Console.WriteLine($"The card you drew was {test} which is higher than {previousCard} meaning you lose 75 points, setting your total to {score}!");
                 }
             }
-            else{
-                Console.WriteLine("That was not a valid answer please either write HIGHER or LOWER");
-            }
+            previousCard = test;
             cardList.RemoveAt(selectedCard);
                
         }
